be72749ca53a443b0ad15bebc2f308c6b0a58780
Amanda Ross <amikaross@gmail.com>
2022-09-14 09:14:21 -0600

    Create book class to pass tests

---
+++ lib/book.rb
@@ -0,0 +1,11 @@
+class Book 
+  attr_reader :title,
+              :author,
+              :publication_year
+
+  def initialize(book_hash)
+    @title = book_hash[:title]
+    @author = book_hash[:author_first_name] + " " + book_hash[:author_last_name]
+    @publication_year = book_hash[:publication_date].slice(-4, 4)
+  end
+end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,24 +3,24 @@ require "./lib/book"
 
 RSpec.describe Book do 
   before(:each) do 
-    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
   end
-  
+
   describe "#initialize" do 
     it "exists" do 
-      expect(book).to be an_instance_of Book
+      expect(@book).to be_an_instance_of Book
     end
 
     it "has a readable title" do 
-      expect(book.title).to eq "To Kill a Mockingbird"
+      expect(@book.title).to eq "To Kill a Mockingbird"
     end
 
     it "has a readable author" do 
-      expect(book.author).to eq "Harper Lee"
+      expect(@book.author).to eq "Harper Lee"
     end
 
     it "has a readable publication year" do 
-      expect(book.publication_year).to eq "1960"
+      expect(@book.publication_year).to eq "1960"
     end
   end
 end
